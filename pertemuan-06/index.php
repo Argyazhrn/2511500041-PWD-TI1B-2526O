9061c0b12becb7506554e85dc5abc8fcb334a061
Argya Zahran Dwiputra Sodikin <argyazzahran5@gmail.com>
2025-11-04 16:02:46 +0700

    variabel php

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -26,22 +26,22 @@
       <h2>Selamat Datang</h2>
       <p>Ini contoh paragraf HTML.</p>
       <?php
-      echo "<p>Halo Dunia</p>",
-      echo "<p>Nama saya Argya Zahran Dwiputra Sodikin</p>",
+        echo "<p>Halo Dunia</p>";
+        echo "<p>Nama saya Argya Zahran Dwiputra Sodikin</p>";
       ?>
     </section>
 
     <?php
-    $NIM              = "2511500041"
-    $Nama             = "Argya Zahran Dwiputra Sodikin"
-    $Tempat_Lahir     = "Bandung"
-    $Tanggal_Lahir    = "18 Agustus 2006"
-    $Pekerjaan        = "Mahasiswa"
-    $Pasangan         = "Tidak ada"
-    $Nama_Ayah        = "Ace Sodikin"
-    $Nama_Ibu         = "Reni Nurhayati"
-    $Nama_Kakak       = "Arya Putra Purnama Sodikin"
-    $Nama_Adik        = "Nasya Lovelia Putri Sodikin"
+      $NIM            = "2511500041";
+      $Nama           = "Argya Zahran Dwiputra Sodikin";
+      $Tempat_Lahir   = "Bandung";
+      $Tanggal_Lahir  = "18 Agustus 2006";
+      $Pekerjaan      = "Mahasiswa";
+      $Pasangan       = "Tidak ada";
+      $Nama_Ayah      = "Ace Sodikin";
+      $Nama_Ibu       = "Reni Nurhayati";
+      $Nama_Kakak     = "Arya Putra Purnama Sodikin";
+      $Nama_Adik      = "Nasya Lovelia Putri Sodikin";
     ?>
 
     <section id="about">
@@ -51,16 +51,15 @@
       <p><strong>Tempat Lahir:</strong> <?php echo $Tempat_Lahir; ?></p>
       <p><strong>Tanggal Lahir:</strong> <?php echo $Tanggal_Lahir; ?></p>
       <p><strong>Pekerjaan:</strong> <?php echo $Pekerjaan; ?></p>
-      <p><strong>Pasangan:</strong> <?php echo $Pasangan &times; ?></p>
+      <p><strong>Pasangan:</strong> <?php echo $Pasangan; ?> &times;</p>
       <p><strong>Nama Ayah:</strong> <?php echo $Nama_Ayah; ?></p>
       <p><strong>Nama Ibu:</strong> <?php echo $Nama_Ibu; ?></p>
       <p><strong>Nama Kakak:</strong> <?php echo $Nama_Kakak; ?></p>
-      <p><strong>Nama Adik:</strong> <?php echo $Nama_Adik; &#128512; ?></p>
+      <p><strong>Nama Adik:</strong> <?php echo $Nama_Adik; ?> &#128512;</p>
     </section>
 
     <section id="contact">
       <h2>Kontak Kami</h2>
-      
       <form action="" method="GET">
         <label for="txtNama">Nama:</label>
         <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan Nama" required autocomplete="name">
@@ -71,7 +70,6 @@
         <label for="txtPesan">Pesan:</label>
         <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
         <small id="charCount">0/200 karakter</small>
-      </label>
 
         <button type="submit">Kirim</button>
         <button type="reset">Batal</button>
@@ -82,7 +80,7 @@
   <footer>
     <p>&copy; 2025 Argya Zahran Dwiputra Sodikin [2511500041]</p>
   </footer>
-  
+
   <script src="script.js"></script>
 </body>
 </html>
