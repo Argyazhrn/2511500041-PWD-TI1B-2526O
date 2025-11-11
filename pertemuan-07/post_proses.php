79b24b3951625d4bb38b11695878fde311756e8d
Argya Zahran Dwiputra Sodikin <argyazzahran5@gmail.com>
2025-11-12 01:02:00 +0700

    menyesuaikan get ke post

--- pertemuan-07/post_proses.php
+++ pertemuan-07/post_proses.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-$_SESSION["nama"]  = $_GET["txtNama"];
-$_SESSION["Email"] = $_GET["txtEmail"];
-$_SESSION["Pesan"] = $_GET["txtPesan"];
-header(header: "location: get.php");
+$_SESSION["nama"]  = $_post["txtNama"];
+$_SESSION["Email"] = $_post["txtEmail"];
+$_SESSION["Pesan"] = $_post["txtPesan"];
+echo $_SESSION["nama"] . $_SESSION["Email"] . $_SESSION["Pesan"];
+#header("location: post.php");
 ?>
